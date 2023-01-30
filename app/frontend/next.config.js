f96cfdb7214e07ad66075046c4b1ff88f7fde254
wagatsuma.yuichi <firefly.of.april@gmail.com>
2023-01-31 04:14:52 +0900

    [add] 5.create-next-app + supabase setup

---
+++ app/frontend/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  // create-next-app --example with-tailwindcssは現在試験段階のため/appディレクトリを有効化するのに下記が必要になる
+  experimental: {
+    appDir: true,
+  }
+}
