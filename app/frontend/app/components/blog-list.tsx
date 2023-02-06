8f7cc6642ce2a0669d411a245284083f90fd4a07
wagatsuma.yuichi <firefly.of.april@gmail.com>
2023-02-06 14:08:41 +0900

    [add] 12.Streaming HTML

---
+++ app/frontend/app/components/blog-list.tsx
@@ -0,0 +1,38 @@
+import type { Database } from '../../database.types'
+
+type Blog = Database['public']['Tables']['blogs']['Row']
+
+async function fetchBlogs() {
+  // フェッチしていることがわかるように遅延を入れる
+  await new Promise((resolve) => setTimeout(resolve, 6000))
+  const res = await fetch(`${process.env.url}/rest/v1/blogs?select=*`, {
+    headers: new Headers({
+      apikey: process.env.apikey as string,
+    }),
+  })
+  if (!res.ok) {
+    throw new Error('Failed to fetch blogs')
+  }
+  const blogs: Blog[] = await res.json()
+
+  return blogs
+}
+
+export default async function BlogList() {
+  const blogs = await fetchBlogs()
+
+  return (
+    <div className='p-4'>
+      <p className='mb-4 pb-4 text-xl font-medium underline underline-offset-4'>
+        Blogs
+      </p>
+      <ul className='text-sm'>
+        {blogs.map((blog) => (
+          <li className='my-1 text-base' key={blog.id}>
+            {blog.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
--- app/frontend/app/components/nav-bar.tsx
+++ app/frontend/app/components/nav-bar.tsx
@@ -5,12 +5,12 @@ export default function NavBar() {
     <header className='bg-gray-800 p-4'>
       <nav className='space-x-4'>
         {/* ランディングページに移動する */}
-        {/* <Link
+        <Link
           href='/'
           className='rounded bg-gray-700 px-3 py-2 text-white hover:bg-gray-500'
         >
           Home
-        </Link> */}
+        </Link>
         {/* 匿名アカウントでも閲覧できるブログに移動する */}
         <Link
           href='/blogs'
